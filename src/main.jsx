a005984712536fb14301102243fa055e52df4702
Debottam Kar <debottamkar2003@gmail.com>
2025-06-09 00:51:08 +0530

    formatting

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,27 @@
 // import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import Home from './home.jsx'
-import Profile from './profile.jsx'
-import Reels from './reels.jsx'
-import Search from './search.jsx'
-import Upload from './upload.jsx'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import Home from "./home.jsx";
+import Profile from "./profile.jsx";
+import Reels from "./reels.jsx";
+import Search from "./search.jsx";
+import Upload from "./upload.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
 const router = createBrowserRouter([
-  
-  {path:'/',element: <App />,children:[
-    {path:'',element: <Home />},
-    {path:'/profile',element: <Profile />},
-    {path:'/reels',element: <Reels />},
-    {path:'/search',element: <Search />},
-    {path:'/upload',element: <Upload />}
-  ]}
-
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "", element: <Home /> },
+      { path: "/profile", element: <Profile /> },
+      { path: "/reels", element: <Reels /> },
+      { path: "/search", element: <Search /> },
+      { path: "/upload", element: <Upload /> },
+    ],
+  },
 ]);
-createRoot(document.getElementById('root')).render(
-<RouterProvider router={router}></RouterProvider>
-)
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router}></RouterProvider>
+);
